2f0a50edc845c93d416438404ddecd6c95535075
Natasha Kang <natashakangg@gmail.com>
2022-04-24 23:30:01 -0400

    complete function playRound

--- myscripts.js
+++ myscripts.js
@@ -2,12 +2,66 @@ function computerPlay(){
     let x = Math.floor(Math.random() * 3);
     switch(x){
         case 0:
-            return "Rock";
+            return 'rock'
         case 1:
-            return "Paper"
+            return 'paper'
         case 2:
-            return "Scissors"
+            return 'scissors'
     }
 }
 
-console.log(computerPlay())
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    let winner
+    switch(playerSelection){
+        case 'rock':
+            switch(computerSelection){
+                case 'rock':
+                    winner='tie'; 
+                    break;
+                case 'paper':
+                    winner='computer';
+                    break;
+                case 'scissors':
+                    winner='player';
+                    break;
+            }
+            break;
+        
+        case 'paper':
+            switch(computerSelection){
+                case 'rock':
+                    winner='player'; 
+                    break;
+                case 'paper':
+                    winner='tie';
+                    break;
+                case 'scissors':
+                    winner='computer';
+                    break;
+            }
+            break;
+
+        case 'scissors':
+            switch(computerSelection){
+                case 'rock':
+                    winner='computer';
+                    break;
+                case 'paper':
+                    winner='player';
+                    break;
+                case 'scissors':
+                    winner='tie';
+                    break;
+            }
+            break;
+        default:
+            winner='invalid entry from player'
+    }
+    return winner;
+}
+
+const computerSelection = computerPlay();
+const playerSelection = prompt("Enter rock, paper or scissors");
+
+console.log(computerSelection+playRound(playerSelection,computerSelection))
